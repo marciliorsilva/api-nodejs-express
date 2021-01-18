930f7115e35771716969166aa2a0d8b5bff217ca
Marcilio <marciliorsilva93@gmail.com>
2021-01-18 13:57:33 -0300

    criado funçao saveUser; criado funcao de rotas(get)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const userRoute = require('./routes/userRoute')
 const app = express()
 const port = 3000
 
+userRoute(app)
 
 app.get('/', (req, res) => res.send('Ola mundo pelo express'))
 app.listen(port, () => console.log("funcionando de boa"))
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,29 @@
 const fs = require('fs')
-const {join} = fs
+const { join } = require('path')
 
-const filePath = 
+const filePath = join(__dirname, 'users.json')
+
+const getUsers = () =>{
+    // verificando se o arquivo existe
+    const data = fs.existsSync(filePath) 
+        ? fs.readFileSync(filePath) : []
+
+    try {
+        return JSON.parse(data)
+    }catch (error){
+        return []
+    }
+}
+
+const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, null, '\t')) 
+
+const userRoute = (app) => {
+    app.route('/users/:id?').get((req,res) => {
+        const users = getUsers()
+
+        res.send({users})
+    }) 
+}
+
+module.exports = userRoute
+
